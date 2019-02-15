45247281b8df07dc98e84bb1144a981b240da196
Dylan Heinrich <harly13@live.com>
2019-02-15 00:19:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 
-function destructivelyaddElementToEndOfArray(list1, e) {
+function destructivelyaddElementToBeginningOfArray(list1, e) {
   return list1.unshift(e)
 }
 
