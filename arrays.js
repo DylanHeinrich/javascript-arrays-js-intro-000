41c80c4368ef25e92fab6370000f5149d83dc51d
Dylan Heinrich <harly13@live.com>
2019-02-15 00:11:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 var chocolatebars = ['snickers', 'hundred', 'grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(list1, e){
+  var list2 = a.unshift(e)
+  return list2
+}
+
+function destructivelyaddElementToEndOfArray(list1,e) {
+  return list1.unshift(e)
+}
